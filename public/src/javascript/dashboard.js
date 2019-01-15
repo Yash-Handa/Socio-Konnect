c85c97cdd1d6fb4540dc7001412a7ac7ebe9f554
Yash Handa <yashhanda7@yahoo.com>
2019-01-15 18:26:21 +0530

    basic dashbord header

--- public/src/javascript/dashboard.js
+++ public/src/javascript/dashboard.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+  const drop = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(drop, {
+    alignment: 'left',
+    constrainWidth: false,
+  });
   const profilePic = document.querySelectorAll('.materialboxed');
   const instances = M.Materialbox.init(profilePic, {});
 });
