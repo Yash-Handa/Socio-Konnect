fb789dc2cbf487983a85d0bba4c76ce4b0b5cc8b
Yash Handa <yashhanda7@yahoo.com>
2019-01-16 04:01:45 +0530

    Added basic scroll effects almost done scrolling

--- public/src/javascript/dashboard.js
+++ public/src/javascript/dashboard.js
@@ -6,13 +6,34 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function() {
   const drop = document.querySelectorAll('.dropdown-trigger');
   const tabs = document.querySelectorAll('.tabs');
+  const profilePic = document.querySelectorAll('.materialboxed');
   M.Dropdown.init(drop, {
     alignment: 'left',
     constrainWidth: false,
   });
+  M.Materialbox.init(profilePic, {});
   M.Tabs.init(tabs, {
     swipeable: false,
   });
-  const profilePic = document.querySelectorAll('.materialboxed');
-  const instances = M.Materialbox.init(profilePic, {});
+});
+
+const me = document.getElementById('me');
+const pic = document.getElementById('pic');
+const name = document.getElementById('name');
+const tab = document.getElementById('tabs');
+
+document.addEventListener("scroll", function() {
+  const scroller = window.scrollY;
+  console.log(scroller);
+  if (scroller < 75) {
+    me.className = 'navigator me';
+    pic.className = 'circle left pic z-depth-3 materialboxed';
+    name.className = 'name';
+    tab.className = 'tabs'
+  } else {
+    me.className ='navigator me me-scroll';
+    pic.className = 'circle left pic pic-scroll z-depth-3 materialboxed';
+    name.className = 'name name-scroll';
+    tab.className = 'tabs tabs-scroll animated fadeInUp faster'
+  }
 });
